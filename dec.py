edef40da4297da2eec163e9203716555d8199e90
Dev-ag12 <123501569+Dev-ag12@users.noreply.github.com>
2023-11-09 13:17:13 +0530

    Update dec.py

--- dec.py
+++ dec.py
@@ -1,1 +1,34 @@
-#lorem ipsum
+
+import os
+from cryptography.fernet import Fernet
+
+key = bytes(b'Devang')
+
+
+def dec():
+    for file in files:
+        with open(file,"rb") as thefile:
+            contents = thefile.read()
+        contents_dec = Fernet(key).decrypt(contents)
+        with open(file,"wb") as thefile:
+            thefile.write(contents_dec)
+            
+
+print("Enter Correct Password to decrypt:")
+
+inc_cnt = 0
+password = input()
+
+def passw(passg,inc_cnt1):
+    if inc_cnt1 == 5:
+        exit()
+    if passg == "devang":
+        dec()
+        print("Congrats All your files have been decrypted")
+    else:
+    inc_cnt1 += 1
+    print("Wrong attempt number:"+str(inc_cnt1)+". Enter Correct password:\n")
+    helm = input()
+    passw(helm,inc_cnt1)
+       
+passw(password,inc_cnt)
